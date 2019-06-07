902503d93c834f4567c2654b8ed61aad89d75bad
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-06-07 22:12:24 +0900

    update

--- flask_server.py
+++ flask_server.py
@@ -1,19 +1,32 @@
 import torchvision.transforms as transforms
+from flask import Flask, render_template, request
 from torchvision.models import resnet50
-
-from flask import Flask, request
 
 app = Flask(__name__)
 
 model = resnet50(pretrained=True)
-normalize = transforms.Normalize()
+model.eval()
+
+preprocess = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
 
 @app.route("/")
 def get_index_html():
-    raise NotImplementedError()
+    return render_template("index.html")
 
 
-@app.route("/predict")
+@app.route("/predict", methods=["POST"])
 def predict_proba():
-    raise NotImplementedError()
+    img = request.files["file"]
+    return type(img)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="::", port=5001)
