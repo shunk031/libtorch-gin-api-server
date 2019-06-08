66ca286c5085b735ec6cef406ea4c6728132d4a5
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2019-06-08 13:19:53 +0900

    update

--- flask_server.py
+++ flask_server.py
@@ -1,5 +1,12 @@
+import datetime
+import json
+from io import BytesIO
+
+import torch
+import torch.nn.functional as F
 import torchvision.transforms as transforms
-from flask import Flask, render_template, request
+from flask import Flask, g, jsonify, render_template, request
+from PIL import Image
 from torchvision.models import resnet50
 
 app = Flask(__name__)
@@ -17,15 +24,44 @@ preprocess = transforms.Compose(
 )
 
 
+def load_label():
+    with open("assets/imagenet_class_index.json", "r") as rf:
+        class_index = json.load(rf)
+    return {int(k): v for k, v in class_index.items()}
+
+
+labels = load_label()
+
+
+@app.before_request
+def before_request():
+    g.request_start_time = datetime.datetime.now()
+
+    delta = datetime.datetime.now() - g.request_start_time
+    g.request_time = int(delta.total_seconds() * 1000)
+
+
 @app.route("/")
 def get_index_html():
     return render_template("index.html")
 
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["GET", "POST"])
 def predict_proba():
-    img = request.files["file"]
-    return type(img)
+    if request.method == "POST":
+        img = Image.open(BytesIO(request.files["file"].read())).convert("RGB")
+        with torch.no_grad():
+            img_tensor = preprocess(img)
+            img_tensor.unsqueeze_(0)
+            out = F.softmax(model(img_tensor), dim=1)
+            max_id = out.argmax()
+            max_prob = out.max()
+
+            app.logger.debug(g.request_time)
+
+            return jsonify(
+                {"description": labels[int(max_id)][1], "score": float(max_prob)}
+            )
 
 
 if __name__ == "__main__":
